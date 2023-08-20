a145aaf8c057e2cfa721fb48f1d95b16b337d7de
ixaxaar <root@ixaxaar.in>
2023-08-20 21:30:34 +0530

    publish and readme

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="streaming_spouts",
+    name="genius_streaming_spouts",
     version="0.1.0",
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=[],
     python_requires=">=3.10",
     author="ixaxaar",
     author_email="ixaxaar@geniusrise.ai",
-    description="TCP spouts for geniusrise",
+    description="Streaming spouts for geniusrise",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ixaxaar/streaming_spouts",
+    url="https://github.com/ixaxaar/streaming-spouts",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
