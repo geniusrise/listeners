3af9fe59786a1fffff6088312d045a31f86c9230
KeertanaSG <gkshivroop@email.com>
2023-11-15 20:55:53 +0530

    Added unit tests for WebSocket

--- tests/test_websocket.py
+++ tests/test_websocket.py
@@ -24,23 +24,6 @@ def mock_websocket(mock_output, mock_state):
     return Websocket(mock_output, mock_state)
 
 
-# Mocking the WebSocket context manager to prevent it from running indefinitely
-@pytest.fixture
-def mock_websocket_serve():
-    mock_ws = mock.MagicMock()
-    mock_ws.serve.return_value.__aenter__ = mock.MagicMock()
-    mock_ws.serve.return_value.__aexit__ = mock.MagicMock()
-    return mock_ws
-
-
-# Tests initialization of the WebSocket with no custom arguments
-def test_websocket_init(mock_output, mock_state):
-    websocket = Websocket(mock_output, mock_state)
-    assert websocket.output == mock_output
-    assert websocket.state == mock_state
-    assert websocket.top_level_arguments == {}
-
-
 # Tests initialization of the WebSocket with custom arguments
 def test_websocket_init_with_args(mock_output, mock_state):
     custom_kwargs = {
@@ -51,14 +34,6 @@ def test_websocket_init_with_args(mock_output, mock_state):
     assert websocket.output == mock_output
     assert websocket.state == mock_state
     assert websocket.top_level_arguments == custom_kwargs
-
-
-# Tests initialization of the WebSocket with an empty custom arguments
-def test_websocket_init_empty_custom_args(mock_output, mock_state):
-    websocket = Websocket(mock_output, mock_state, **{})
-    assert websocket.output == mock_output
-    assert websocket.state == mock_state
-    assert websocket.top_level_arguments == {}
 
 
 # Tests initialization of the WebSocket with non-string custom arguments
@@ -83,14 +58,12 @@ def test_receive_message_successful(mock_websocket):
         "failure_count": 0,
     }
     mock_websocket.state.set_state = mock.MagicMock()
-
-    websocket_mock = mock.MagicMock()
-    websocket_mock.recv = AsyncMock(return_value="sample_data")
-    websocket_mock.remote_address = ("127.0.0.1", 8765)
+    mock_websocket.recv = AsyncMock(return_value="sample_data")
+    mock_websocket.remote_address = ("127.0.0.1", 8765)
     path = "/test"
 
     # Simulating the async call to receive_message method
-    asyncio.run(mock_websocket.receive_message(websocket_mock, path))
+    asyncio.run(mock_websocket.receive_message(mock_websocket, path))
 
     # Assertions to ensure correct methods were called
     expected_data = {
@@ -112,14 +85,12 @@ def test_receive_message_failure(mock_websocket):
         "failure_count": 0,
     }
     mock_websocket.state.set_state = mock.MagicMock()
-
-    websocket_mock = mock.MagicMock()
-    websocket_mock.recv = AsyncMock(side_effect=Exception("Error!"))
-    websocket_mock.remote_address = ("127.0.0.1", 8765)
+    mock_websocket.recv = AsyncMock(side_effect=Exception("Error!"))
+    mock_websocket.remote_address = ("127.0.0.1", 8765)
     path = "/test"
 
     # Simulating the async call to receive_message method
-    asyncio.run(mock_websocket.receive_message(websocket_mock, path))
+    asyncio.run(mock_websocket.receive_message(mock_websocket, path))
 
     # Assertions to ensure correct methods were called or not called
     mock_websocket.output.save.assert_not_called()
